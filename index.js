0163b5230002a0158a5c55154b0bfcc64bac5572
Mae Schreck <maedevlin47@gmail.comm>
2022-10-12 21:11:52 -0400

    commit tree 55a8049761639ade9c66455bf00836b26c467a01

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your code here!
+//remove <main id="main"></main> within body
+
+const element = document.getElementById('main');
+element.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Mae is the champion";
+//document.body.append(element);
+
+//const newHeader = document.createElement('newHeader');
+//h1.append(newHeader);
+//document.h1.append(newHeader);
+//function addElement() {
+//const newHeader = document.createElement('h1');
+//}
+
+//element.className ="newHeader";
+
+//h1.innerHTML ="new Header";
+
+//h1.textContent = "newHeader";
+//console.log(h1.textContent);
+
+//let h1 = document.createElement("h1")
+////div.append("newHeader")
+
+//console.log(h1.textContent)
+
